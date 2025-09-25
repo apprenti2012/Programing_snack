4cacea14de8542ac8d41be3041e1978d0e42b052
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-25 16:44:06 +0200

    Update mlt.py

--- mlt.py
+++ mlt.py
@@ -1,17 +1,19 @@
 import random
 scr=0
 for i in range(10):
+  random.seed()
   m1=random.randint(0,10)
+  random.seed()
   m2=random.randint(0,10)
-  print(m1," * ",m2, end=" ")
-  rep=int(input("=", end=" "))
+  print(m1,"*",m2, end=" ")
+  rep=int(input("= "))
   res=m1*m2
   if rep==res:
     print("juste")
     scr+=1
   else:
     print("faux")
-    print(str(res))
+    print(f"la vraie réponse était: {str(res)}")
 if scr==10:
   print("Bravo tu as fait u score parfait : ", scr, "/10")
 else:
