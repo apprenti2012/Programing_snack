518e33d3724498acbf8848a2645f5121e7d61fd8
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-26 09:48:20 +0200

    Update mlt.py

--- mlt.py
+++ mlt.py
@@ -19,4 +19,56 @@ def lev1() :
     print("Bravo tu as fait un score parfait : ", scr, "/10")
   else:
     print("Voilà ton score : ", scr, "/10")
-level=1
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def lev2() :
+  for i in range(20):
+    random.seed()
+    m1=random.randint(0,11)
+    random.seed()
+    m2=random.randint(0,10)
+    print(m1,"*",m2, end=" ")
+    rep=int(input("= "))
+    res=m1*m2
+    if rep==res:
+      print("juste")
+      scr+=1
+    else:
+      print("faux")
+      print(f"la vraie réponse était: {str(res)}")
+  if scr==10:
+    print("Bravo tu as fait u score parfait : ", scr, "/10")
+  else:
+    print("Voilà ton score : ", scr, "/10")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def lev3() :
+  for i in range(30):
+    random.seed()
+    m1=random.randint(6,12)
+    random.seed()
+    m2=random.randint(6,12)
+    print(m1,"*",m2, end=" ")
+    rep=int(input("= "))
+    res=m1*m2
+    if rep==res:
+      print("juste")
+      scr+=1
+    else:
+      print("faux")
+      print(f"la vraie réponse était: {str(res)}")
+  if scr==10:
+    print("Bravo tu as fait u score parfait : ", scr, "/10")
+  else:
+    print("Voilà ton score : ", scr, "/10")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+level=input("choisi ton niveau : 1,2 ou 3, (Tape 1, 2 ou 3 ")
+if level=="1":
+  lev1()
+elif level=="2":
+  lev2()
+else:
+  lev3()
+  
+  
