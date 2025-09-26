236e6fa34783d6166d5c983687f26af6c0bd59d6
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-26 13:46:12 +0200

    Update mlt.py

--- mlt.py
+++ mlt.py
@@ -2,33 +2,29 @@ import random
 def lev1():
     scr=0
     for i in range(10):
-        random.seed()
         m1=random.randint(0,10)
-        random.seed()
         m2=random.randint(0,10)
-        print(m1,"*",m2, end=" ")
-        rep=int(input("= "))
+        print(f"{m1} * {m2}")
+        rep=int(input("="))
         res=m1*m2
         if rep==res:
-            print("juste")
-            scr+=1
+          print("juste")
+          scr+=1
         else:
-            print("faux")
-            print(f"la vraie réponse était: {str(res)}")
-        if scr==10:
-            print("Bravo tu as fait un score parfait : ", scr, "/10")
-        else:
-            print("Voilà ton score : ", scr, "/10")
+          print("faux")
+          print(f"la vraie réponse était: {str(res)}")
+    if scr==10:
+      print("Bravo tu as fait un score parfait : ", scr, "/10")
+    else:
+      print("Voilà ton score : ", scr, "/10")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def lev2():
   scr=0
-  for i in range(20):
-    random.seed()
+  for i in range(20)
     m1=random.randint(0,11)
-    random.seed()
     m2=random.randint(0,10)
-    print(m1,"*",m2, end=" ")
+    print(f"{m1} * {m2}")
     rep=int(input("= "))
     res=m1*m2
     if rep==res:
@@ -37,20 +33,18 @@ def lev2():
     else:
       print("faux")
       print(f"la vraie réponse était: {str(res)}")
-  if scr==10:
+  if scr==20:
     print("Bravo tu as fait un score parfait : ", scr, "/10")
   else:
-    print("Voilà ton score : ", scr, "/10")
+    print("Voilà ton score : ", scr, "/20")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def lev3():
   scr=0
   for i in range(30):
-    random.seed()
     m1=random.randint(6,12)
-    random.seed()
     m2=random.randint(6,12)
-    print(m1,"*",m2, end=" ")
+    print(f"{m1} * {m2}")
     rep=int(input("= "))
     res=m1*m2
     if rep==res:
@@ -59,10 +53,10 @@ def lev3():
     else:
       print("faux")
       print(f"la vraie réponse était: {str(res)}")
-  if scr==10:
+  if scr==30:
     print("Bravo tu as fait un score parfait : ", scr, "/10")
   else:
-    print("Voilà ton score : ", scr, "/10")
+    print("Voilà ton score : ", scr, "/30")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 level=input("choisi ton niveau : 1,2 ou 3, (Tape 1, 2 ou 3): ")
