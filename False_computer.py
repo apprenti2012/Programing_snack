e81e9a0e063ae66a3c1f3b83269e4b95ad59f634
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-28 12:18:25 +0200

    Create False_computer.py

---
+++ False_computer.py
@@ -0,0 +1,11 @@
+import random, time
+print("#create_new_competer")
+time.sleep(5)
+for y in range(0,50)
+  for x in range(0,20):
+    i=random.randint(0,1)
+    print(str(i) end="")
+  time.sleep(random.randint(3,6))
+print("end session")
+  
+      
