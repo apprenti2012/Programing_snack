66728639cec9075ca68d804948d0c580676581d0
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-29 08:34:46 +0200

    Update False_computer.py

--- False_computer.py
+++ False_computer.py
@@ -1,11 +1,13 @@
 import random, time
-print("#create_new_competer")
+print("#create_new_computer")
 time.sleep(5)
 for y in range(0,50)
-  for x in range(0,20):
+  for x in range(0,19):
     i=random.randint(0,1)
     print(str(i) end="")
   time.sleep(random.randint(3,6))
+print(str(i))
+print("===========")
 print("end session")
-  
+print("===========") 
       
