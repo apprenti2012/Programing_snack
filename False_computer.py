09979075c4a50cda87cafad770e18256ebd40cfa
apprenti2012 <munoz.yannis04@gmail.com>
2025-09-29 08:45:23 +0200

    Update False_computer.py

--- False_computer.py
+++ False_computer.py
@@ -4,7 +4,8 @@ time.sleep(5)
 for y in range(0,50):
   for x in range(0,19):
     i=random.randint(0,1)
-    print(str(i))
+    print(str(i),end="")
+  print(str(i)
   time.sleep(random.randint(3,6))
 print("===========")
 print("end session")
