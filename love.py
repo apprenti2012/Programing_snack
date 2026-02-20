896004e17fc77169860b4467f31a7a10d04233d6
apprenti2012 <munoz.yannis04@gmail.com>
2026-02-20 13:26:16 +0000

    apprenti2012: prototype

--- love.py
+++ love.py
@@ -1,8 +1,11 @@
-import random
-name1 = input("entrer le prénom de la personne que vous aimez : ")
-name2 = input("entrer votre prénom : ")
-nombre = random.randint(1,3)
-if nombre == 1 :
-	print(f"{name1} et vous ({name2}) n'êtes pas fait pour être ensemble")
-else:
-	print(f"{name1} et vous ({name2}) allez vivre heureux pour toujours 😊") 
+import random 
+def two_version():
+ name1 = input("entrer le prénom de la personne que vous aimez : ")
+ name2 = input("entrer votre prénom : ")
+ nombre = random.randint(1,3)
+ if nombre == 1 :
+	 print(f"{name1} et vous ({name2}) n'êtes pas fait pour être ensemble")
+ else:
+	 print(f"{name1} et vous ({name2}) allez vivre heureux pour toujours 😊")
+def random_version():
+	woman_list = 
